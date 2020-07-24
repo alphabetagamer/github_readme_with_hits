ee7a282469a795350e5764f555bc42021b317900
lokinsethia <alphabetagamma42@gmail.com>
2020-07-24 10:52:42 +0530

    logs

--- src/views.js
+++ src/views.js
@@ -13,12 +13,18 @@ function check(rows,namerepo){
     return new Promise(function(res,rej){
     if(rows.length!=0){
         db.run("UPDATE views SET hits = hits + 1 WHERE repo ='"+namerepo+"'",(err,row)=>{
+            if(err){
+                logger.log("error in check/update",err);
+            }
                     // console.log('updated')
                     res(rows[0].hits+1)
     })
 }
     else{
         db.run("INSERT INTO views (repo) VALUES (?)",[namerepo],(err,rows)=>{
+                if(err){
+                    logger.log("error in check/insert",err);
+                }
                     // console.log('newinsert')
                     // console.log(namerepo)
                     res(1)
@@ -29,6 +35,9 @@ function check(rows,namerepo){
 function hits(namerepo){
     return new Promise(function(res,rej){
         db.all("SELECT hits FROM views WHERE repo ='"+namerepo+"'",(err,rows)=>{
+            if(err){
+                logger.log("error in hits",err);
+            }
             // console.log(rows.length)
             res(check(rows,namerepo))            
         })
@@ -38,8 +47,10 @@ const final_hits=async function(namerepo){
     logger.log("In final_hits");
         
 return new Promise(function(res,rej){
+
 db.run(`CREATE TABLE IF NOT EXISTS views(id INTEGER PRIMARY KEY AUTOINCREMENT,repo TEXT UNIQUE,hits INTEGER DEFAULT 1)`,(err,pass)=>{
 if(err){
+    logger.log("error in final_hits",err);
     // console.log(err);
     db.close()
     res(-1)
